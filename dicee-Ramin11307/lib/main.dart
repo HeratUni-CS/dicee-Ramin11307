31cd2b0bd3acb0c2c817331183d1baa38c980661
Ramin11307 <129157654+Ramin11307@users.noreply.github.com>
2023-06-07 01:49:42 -0700

    first commit

---
+++ dicee-Ramin11307/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(title: Center(child: Text('Dice'),), backgroundColor: Colors.red,
+      ),
+      body:Dicepage(),
+    ),
+  ));
+}
+class Dicepage extends StatefulWidget {
+  const Dicepage({Key? key}) : super(key: key);
+
+  @override
+  State<Dicepage> createState() => _DicepageState();
+}
+
+class _DicepageState extends State<Dicepage> {
+  int leftDicenumber=1;
+  int rightDicenumber=2;
+  void ChangeDice(){
+    setState(() {
+      rightDicenumber=Random().nextInt(6) +1;
+      leftDicenumber=Random().nextInt(6) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Row(
+        children:[
+          Expanded(
+            child:TextButton(onPressed: () {
+              ChangeDice();
+            },
+              child: Padding( padding:EdgeInsets.all(15),child:Image.asset('images/Dice$leftDicenumber.png'),),
+            ),
+          ),
+          Expanded(
+            child:TextButton(onPressed: (){
+              ChangeDice();
+            },
+                child:Padding(padding: EdgeInsets.all(15),child: Image.asset('images/Dice$rightDicenumber.png'),
+                )
+
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
